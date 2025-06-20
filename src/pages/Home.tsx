b10abc6ad0928e69b96207bff9602e3d961b72f6
NotABot <135363764+BryanTheLai@users.noreply.github.com>
2025-06-20 18:50:21 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -130,7 +130,7 @@ export default function Home() {
           </p>
 
           {/* Partners Section */}
-          <div id="contact" className="mt-16">
+          {/* <div id="contact" className="mt-16">
             <h3 className="text-2xl font-semibold mb-8 text-slate-500">
               Our Partners
             </h3>
@@ -148,7 +148,7 @@ export default function Home() {
             <p className="text-slate-400 mt-8">
               Future partners' logos will appear here
             </p>
-          </div>
+          </div> */}
         </div>
       </section>
 
